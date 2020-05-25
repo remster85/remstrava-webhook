a884a0f62cd0fcca74e054a8028d78cac2b7f781
remster85 <remi.desreumaux@gmail.com>
2020-05-25 18:10:46 -0400

    First commit

---
+++ __init__.py
@@ -0,0 +1,19 @@
+import logging
+import json
+import azure.functions as func
+import os
+from azure.eventhub import EventHubProducerClient, EventData
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+        
+    connection_str = os.environ.get("EventHubConnectionString")
+    eventhub_name = 'newactivityhub'
+    
+    client  = EventHubProducerClient.from_connection_string(connection_str, eventhub_name=eventhub_name)
+    
+    event_data_batch = client.create_batch()
+    event_data_batch.add(EventData('Message inside EventBatchData'))
+    
+    client.send_batch(event_data_batch)
+    
