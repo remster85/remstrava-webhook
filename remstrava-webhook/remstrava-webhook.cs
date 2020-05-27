c2328ed0cc3bd8731a1f6b79039b0b0d11656116
remster85 <remi.desreumaux@gmail.com>
2020-05-27 08:38:34 -0400

    Use DorNet soluution

---
+++ remstrava-webhook/remstrava-webhook.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Azure.EventHubs;
+using System.Text;
+
+namespace remstrava_webhook
+{
+    public static class NewActivity
+    {
+
+        private static EventHubClient eventHubClient;
+        private const string EventHubName = "newactivity";
+
+        [FunctionName("remstrava-webhook")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+
+            MainAsync().GetAwaiter().GetResult();
+
+            string responseMessage = "OK";
+
+            return new OkObjectResult(responseMessage);
+        }
+
+
+        private static async Task MainAsync()
+        {
+            // Creates an EventHubsConnectionStringBuilder object from the connection string, and sets the EntityPath.
+            // Typically, the connection string should have the entity path in it, but for the sake of this simple scenario
+            // we are using the connection string from the namespace.
+            var connectionStringBuilder = new EventHubsConnectionStringBuilder(System.Environment.GetEnvironmentVariable("EventHubConnectionString", EnvironmentVariableTarget.Process))
+            {
+                EntityPath = EventHubName
+            };
+
+            eventHubClient = EventHubClient.CreateFromConnectionString(connectionStringBuilder.ToString());
+
+            await SendMessagesToEventHub(1);
+
+            await eventHubClient.CloseAsync();
+
+            Console.WriteLine("Press ENTER to exit.");
+            Console.ReadLine();
+        }
+
+        // Uses the event hub client to send 100 messages to the event hub.
+        private static async Task SendMessagesToEventHub(int numMessagesToSend)
+        {
+            for (var i = 0; i < numMessagesToSend; i++)
+            {
+                try
+                {
+                    var message = $"Message {i}";
+                    Console.WriteLine($"Sending message: {message}");
+                    await eventHubClient.SendAsync(new EventData(Encoding.UTF8.GetBytes(message)));
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine($"{DateTime.Now} > Exception: {exception.Message}");
+                }
+
+                await Task.Delay(10);
+            }
+
+            Console.WriteLine($"{numMessagesToSend} messages sent.");
+        }
+    }
+}
